b52ceba467d5dc5308e9cf347e8799a21dea6e48
winter <zwtao90@gmail.com>
2025-08-31 11:49:43 +0800

    添加代码高亮

--- src/Post.vue
+++ src/Post.vue
@@ -8,12 +8,25 @@
 <script setup>
 import { ref } from "vue";
 import { useRoute } from "vue-router";
-import { marked } from "marked";
+import { Marked } from "marked";
+import { markedHighlight } from "marked-highlight";
+import hljs from "highlight.js";
 import { parseFrontmatter } from "./utils";
 
 const route = useRoute();
-
 const path = route.params.post;
+
+const marked = new Marked(
+  markedHighlight({
+    emptyLangClass: "hljs",
+    langPrefix: "hljs language-",
+    highlight(code, lang, info) {
+      const language = hljs.getLanguage(lang) ? lang : "plaintext";
+      return hljs.highlight(code, { language }).value;
+    },
+  })
+);
+
 const md = ref("");
 const meta = ref({});
 
